ddd548dc4cbef3ae9a2488a1d60c2598d0d347e1
terminator637 <terminator637@gmail.com>
2018-03-12 21:43:35 +0300

    initial commit

---
+++ signer.go
@@ -0,0 +1,17 @@
+package main
+
+// сюда писать код
+
+func ExecutePipeline(jobs []job) {
+
+}
+
+func SingleHash(in, out chan interface{}) {
+
+}
+func MultiHash(in, out chan interface{}) {
+
+}
+func CombineResults(in, out chan interface{}) {
+
+}
