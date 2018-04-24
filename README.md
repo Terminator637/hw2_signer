3e55a7b9541d352e157ba719d0c5f652d2ff034f
terminator <terminator@iageengineering.net>
2018-04-24 15:46:47 +0300

    Все тесты проходят ~2.07s, + правильный последовательный вывод.

--- README.md
+++ README.md
@@ -1,34 +1,5 @@
 # hw2_signer
 
-Результаты, которые выводятся если отправить 2 значения (закомментировано в тесте):
+Решение задачи **Конвейер функций**. Coursera, курс: [Разработка веб-сервисов на Go - основы языка.](https://www.coursera.org/learn/golang-webservices-1/home/welcome)
 
-```
-0 SingleHash data 0
-0 SingleHash md5(data) cfcd208495d565ef66e7dff9f98764da
-0 SingleHash crc32(md5(data)) 502633748
-0 SingleHash crc32(data) 4108050209
-0 SingleHash result 4108050209~502633748
-4108050209~502633748 MultiHash: crc32(th+step1)) 0 2956866606
-4108050209~502633748 MultiHash: crc32(th+step1)) 1 803518384
-4108050209~502633748 MultiHash: crc32(th+step1)) 2 1425683795
-4108050209~502633748 MultiHash: crc32(th+step1)) 3 3407918797
-4108050209~502633748 MultiHash: crc32(th+step1)) 4 2730963093
-4108050209~502633748 MultiHash: crc32(th+step1)) 5 1025356555
-4108050209~502633748 MultiHash result: 29568666068035183841425683795340791879727309630931025356555
-
-1 SingleHash data 1
-1 SingleHash md5(data) c4ca4238a0b923820dcc509a6f75849b
-1 SingleHash crc32(md5(data)) 709660146
-1 SingleHash crc32(data) 2212294583
-1 SingleHash result 2212294583~709660146
-2212294583~709660146 MultiHash: crc32(th+step1)) 0 495804419
-2212294583~709660146 MultiHash: crc32(th+step1)) 1 2186797981
-2212294583~709660146 MultiHash: crc32(th+step1)) 2 4182335870
-2212294583~709660146 MultiHash: crc32(th+step1)) 3 1720967904
-2212294583~709660146 MultiHash: crc32(th+step1)) 4 259286200
-2212294583~709660146 MultiHash: crc32(th+step1)) 5 2427381542
-2212294583~709660146 MultiHash result: 4958044192186797981418233587017209679042592862002427381542
-
-CombineResults 29568666068035183841425683795340791879727309630931025356555_4958044192186797981418233587017209679042592862002427381542
-
-```
+Все тесты успешно проходятся, гонок нет, время выполнения ~2.07s.
--- signer.go
+++ signer.go
@@ -5,63 +5,101 @@ import (
 	"sort"
 	"strconv"
 	"strings"
-	"time"
+	"sync"
+	"runtime"
 )
 
-
-// сюда писать код
+const TH = 6
 
 var ExecutePipeline = func(jobs ...job) {
-	input := make(chan interface{})
-	in := input
+	wg := &sync.WaitGroup{}
+	in := make(chan interface{})
 	for _, job := range jobs {
+		wg.Add(1)
 		out := make(chan interface{})
-		go job(in, out) // run job() as a goroutine
-		in = out        // the output of one job is the input of the next one
+		go jobWorker(job, in, out, wg)
+		in = out
 	}
-	close(input)
-	time.Sleep(60 * time.Second)
+	wg.Wait()
+}
+
+func jobWorker(job job, in, out chan interface{}, wg *sync.WaitGroup) {
+	defer wg.Done()
+	defer close(out)
+	job(in, out)
 }
 
 var SingleHash = func(in, out chan interface{}) {
+	mu := &sync.Mutex{}
+	wg := &sync.WaitGroup{}
 	for i := range in {
-		data := strconv.Itoa(i.(int))
-		md5Data := DataSignerMd5(data)
-		crc32Md5Data := DataSignerCrc32(md5Data)
-		crc32Data := DataSignerCrc32(data)
-		fmt.Printf("%s SingleHash data %s\n", data, data)
-		fmt.Printf("%s SingleHash md5(data) %s\n", data, md5Data)
-		fmt.Printf("%s SingleHash crc32(md5(data)) %s\n", data, crc32Md5Data)
-		fmt.Printf("%s SingleHash crc32(data) %s\n", data, crc32Data)
-		fmt.Printf("%s SingleHash result %s\n", data, crc32Data+"~"+crc32Md5Data)
-		out <- crc32Data + "~" + crc32Md5Data
+		wg.Add(1)
+		go singleHashWorker(i, out, wg, mu)
 	}
+	wg.Wait()
+}
+
+func singleHashWorker(in interface{}, out chan interface{}, wg *sync.WaitGroup, mu *sync.Mutex) {
+	defer wg.Done()
+	data := strconv.Itoa(in.(int))
+	mu.Lock()
+	md5Data := DataSignerMd5(data)
+	mu.Unlock()
+	crc32DataChan := make(chan string)
+	go crc32Parallel(data, crc32DataChan)
+	crc32Md5Data := DataSignerCrc32(md5Data)
+	crc32Data := <- crc32DataChan
+	fmt.Printf("%s SingleHash data %s\n", data, data)
+	fmt.Printf("%s SingleHash md5(data) %s\n", data, md5Data)
+	fmt.Printf("%s SingleHash crc32(md5(data)) %s\n", data, crc32Md5Data)
+	fmt.Printf("%s SingleHash crc32(data) %s\n", data, crc32Data)
+	fmt.Printf("%s SingleHash result %s\n", data, crc32Data+"~"+crc32Md5Data)
+	out <- crc32Data + "~" + crc32Md5Data
+	runtime.Gosched()
+
+}
+
+func crc32Parallel(data string, out chan string) {
+	out <- DataSignerCrc32(data)
 }
 
 var MultiHash = func(in, out chan interface{}) {
+	wg := &sync.WaitGroup{}
 	for i := range in {
-		var sum = ""
-		for j := 0; j < 6; j++ {
-			crc32 := DataSignerCrc32(strconv.Itoa(j) + i.(string))
-			fmt.Printf("%s MultiHash: crc32(th+step1)) %d %s\n", i.(string), j, crc32)
-			sum += crc32
-		}
-
-		fmt.Printf("%s MultiHash result:\n%s\n", i.(string), sum)
-		out <- sum
+		wg.Add(1)
+		go multiHashWorker(i.(string), out, wg)
 	}
+	wg.Wait()
+}
 
+func multiHashWorker(in string, out chan interface{}, wg *sync.WaitGroup) {
+	defer wg.Done()
+	mu := &sync.Mutex{}
+	wgCrc32 := &sync.WaitGroup{}
+	concatArray := make([]string, TH)
+	for j := 0; j < TH; j++ {
+		wgCrc32.Add(1)
+		data := strconv.Itoa(j) + in
+		go func(concatArray []string, data string, index int, wg *sync.WaitGroup, mu *sync.Mutex) {
+			defer wg.Done()
+			data = DataSignerCrc32(data)
+			mu.Lock()
+			concatArray[index] = data
+			fmt.Printf("%s MultiHash: crc32(th+step1)) %d %s\n", in, index, data)
+			mu.Unlock()
+		}(concatArray, data, j, wgCrc32, mu)
+		runtime.Gosched()
+	}
+	wgCrc32.Wait()
+	result := strings.Join(concatArray,"")
+	fmt.Printf("%s MultiHash result: %s\n", in, result)
+	out <- result
 }
 
 var CombineResults = func(in, out chan interface{}) {
-	var count = 0
 	var array []string
 	for i := range in {
-		count++
 		array = append(array, i.(string))
-		if count > 6 {
-		close(in)
-		}
 	}
 	sort.Strings(array)
 	result := strings.Join(array, "_")
